947e46d1aa6b4927872d7cb2fb4d834376dc163a
6devi6thesh6 <programmistalexandr67@gmail.com>
2023-10-19 20:20:46 +0300

    registr v2

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Регистрационная форма</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/null.css">
     <link rel="stylesheet" href="css/modal.css">
     <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
-    <div class="container">
+    <div class="container" id="container-vid">
         <button class="log_up" id="open-modal-btn">Вход/Регистрация</button>
 
     </div>
@@ -25,9 +28,8 @@
                     <path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" />
                 </svg>
             </button>
-            <h2 class="name_log-up"><span>Вход</span><span>Регистрация</span></h2>
             <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
-            <label for="reg-log"></label>
+            <label for="reg-log" id="in-up"><span class="in-up" id="t-in-up"></span></label>
             <div class="card-3d-wrap mx-auto">
                 <?php
                 if ($_COOKIE['useret'] == '') :
@@ -35,6 +37,7 @@
                     <div class="card-3d-wrapper">
                         <div class="card-front">
                             <div class="center-wrap">
+                            <h2 class="vhod">Вход</h2>  
                                 <div class="section text-center">
                                     <form action="login.php" method="post" class="form2">
                                         <div class="form-group"><input type="text" placeholder="Логин" name="login" class="login"></div>
@@ -47,13 +50,14 @@
                         </div>
                         <div class="card-back">
                             <div class="center-wrap">
+                            <h2 class="reg-b">Регистрация</h2> 
                                 <div class="section text-center">
                                     <form action="register.php" method="post">
                                         <div class="form-group"><input type="text" placeholder="Логин" name="login" class="login"><br></div>
                                         <div class="form-group"><input type="text" placeholder="Пароль" name="pass" class="pass"><br></div>
                                         <div class="form-group"><input type="text" placeholder="Повторите пароль" name="repeatpass" class="repeatpass"><br></div>
                                         <div class="form-group"><input type="text" placeholder="email" name="email" class="email"><br></div>
-                                        <button type="submit" class="reg">Зарегистрироваться</button>
+                                        <button type="submit" class="btn-reset reg">Зарегистрироваться</button>
                                     </form>
                                 </div>
                             </div>
--- js/log_Up.js
+++ js/log_Up.js
@@ -1,6 +1,20 @@
-document.getElementById("open-modal-btn").addEventListener("click", function(){
+document.getElementById("open-modal-btn").addEventListener("click", function () {
     document.getElementById("log-modal").classList.add("open")
+    document.getElementById("container-vid").classList.add("not-vid")
 })
-document.getElementById("close-my-modal-btn").addEventListener("click", function(){
+document.getElementById("close-my-modal-btn").addEventListener("click", function () {
     document.getElementById("log-modal").classList.remove("open")
+    document.getElementById("container-vid").classList.remove("not-vid")
+})
+
+var sigin = true;
+document.getElementById("in-up").addEventListener("click", function () {
+    if (sigin) {
+        document.getElementById("t-in-up").classList.add("in")
+        sigin = false
+    }
+    else {
+        document.getElementById("t-in-up").classList.remove("in")
+        sigin = true
+    }
 })
